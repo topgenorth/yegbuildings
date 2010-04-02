5503d33ce30a8d8a54938c558effd06694a8efd6
Tom Opgenorth <tom@opgenorth.net>
2010-04-01 21:27:39 -0600

    Added in a MyLocationOverlay.

--- src/net/opgenorth/yeg/views/BuildingMap.java
+++ src/net/opgenorth/yeg/views/BuildingMap.java
@@ -4,34 +4,44 @@ import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.widget.Toast;
-import com.google.android.maps.ItemizedOverlay;
-import com.google.android.maps.MapActivity;
-import com.google.android.maps.MapView;
-import com.google.android.maps.OverlayItem;
+import com.google.android.maps.*;
 import net.opgenorth.yeg.R;
-import net.opgenorth.yeg.model.LatLongLocation;
 import net.opgenorth.yeg.widget.GoogleMapPin;
 
 
 public class BuildingMap extends MapActivity {
+	private MyLocationOverlay _myLocation;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.building_map);
+
 		MapView map = (MapView) findViewById(R.id.map);
 		map.getController().setZoom(17);
-
-		LatLongLocation location = new LatLongLocation(getIntent());
-
-		map.getController().setCenter(location.createGeoPoint());
 		map.setBuiltInZoomControls(true);
+		_myLocation = new MyLocationOverlay(this, map);
 
-		Drawable marker = getResources().getDrawable(R.drawable.marker);
-		marker.setBounds(0, 0, marker.getIntrinsicWidth(), marker.getIntrinsicHeight());
+		Drawable buildingMarker = getResources().getDrawable(R.drawable.marker);
+		buildingMarker.setBounds(0, 0, buildingMarker.getIntrinsicWidth(), buildingMarker.getIntrinsicHeight());
 
-		GoogleMapPin pin = new GoogleMapPin(getIntent(), marker);
+		GoogleMapPin pin = new GoogleMapPin(getIntent(), buildingMarker);
+		map.getController().setCenter(pin.getGeoPoint());
+
+		map.getOverlays().add(_myLocation);
 		map.getOverlays().add(new StatusOverlay(pin));
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		_myLocation.enableCompass();
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		_myLocation.disableCompass();
 	}
 
 	@Override
