a95ee0e45aa166ebdf6a852610fc631ef622e076
Tom Opgenorth <tom@opgenorth.net>
2010-08-25 16:18:15 -0600

    * Added another file to .gitignore

--- helpers.ps1
+++ helpers.ps1
@@ -1,7 +1,6 @@
 ﻿$android_sdk_dir = "C:\android-sdk-windows";
+$putty = "C:\Program Files\PuTTY\putty.exe";
 $adb =  $android_sdk_dir + "\tools\adb.exe";
 $db_location = "/data/data/net.opgenorth.yeg.buildings/databases/buildings.db";
-
-
 
 Set-Alias adb "C:\android-sdk-windows\tools\adb.exe" # pull /data/data/net.opgenorth.net.buildings.databases.buildings.db"
