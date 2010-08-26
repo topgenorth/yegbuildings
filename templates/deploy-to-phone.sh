e7f3ca6f1a2acbff2e7cd8be02c0dc085717491d
Tom Opgenorth <tom@opgenorth.net>
2010-08-25 20:47:09 -0600

    * Updated the deploy template

--- templates/deploy-to-phone.sh
+++ templates/deploy-to-phone.sh
@@ -1,5 +1,4 @@
 #/bin/sh
-
 # use this to deploy the application to your phone.  Just replace <DEVICENAME> with the name of your phone.
-adb -s <DEVICENAME> uninstall net.opgenorth.yeg
-adb -s <DEVICENAME> install ./HistoricalBuildings.apk
+adb -s <DEVICENAME> uninstall net.opgenorth.yeg.buildings
+adb -s <DEVICENAME> install ./YegBuildings.apk
