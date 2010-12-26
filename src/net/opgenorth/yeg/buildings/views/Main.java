be0e9f2a3a0699c54c5ebf2dcf962c81bf297a68
Tom Opgenorth <tom@opgenorth.net>
2010-12-26 11:26:49 -0700

    removed the old Activities

---
+++ src/net/opgenorth/yeg/buildings/views/Main.java
@@ -0,0 +1,10 @@
+package net.opgenorth.yeg.buildings.views;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Main extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
