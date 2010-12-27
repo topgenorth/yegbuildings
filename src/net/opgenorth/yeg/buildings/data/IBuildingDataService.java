f1ccd7b16331c3d9df8f67eeed6539dcc30d5525
Tom Opgenorth <tom@opgenorth.net>
2010-12-26 22:21:06 -0700

    adding the IBuildingDataService back in

---
+++ src/net/opgenorth/yeg/buildings/data/IBuildingDataService.java
@@ -0,0 +1,11 @@
+package net.opgenorth.yeg.buildings.data;
+
+import net.opgenorth.yeg.buildings.model.Building;
+import net.opgenorth.yeg.buildings.model.IBuildingSorter;
+
+import java.util.List;
+
+public interface IBuildingDataService {
+    List<Building> fetchAll();
+    List<Building> fetchAll(IBuildingSorter sortedBy);
+}
