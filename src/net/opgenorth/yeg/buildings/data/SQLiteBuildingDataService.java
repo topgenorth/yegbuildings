40ceed8ba4c55325ae40b3f65002bf65f816ee32
Tom Opgenorth <tom@opgenorth.net>
2010-12-27 20:27:42 -0700

    Fixed a bug with how the SQLiteBuildingDataService converts a cursor row to a Building.

--- src/net/opgenorth/yeg/buildings/data/SQLiteBuildingDataService.java
+++ src/net/opgenorth/yeg/buildings/data/SQLiteBuildingDataService.java
@@ -11,74 +11,76 @@ import java.util.UUID;
 
 
 public class SQLiteBuildingDataService implements IBuildingDataService {
-    private ContextWrapper _context;
+	private ContextWrapper _context;
 
-    public SQLiteBuildingDataService(ContextWrapper context) {
-        this._context = context;
-    }
+	public SQLiteBuildingDataService(ContextWrapper context) {
+		this._context = context;
+	}
 
-    @Override
-    public List<Building> fetchAll() {
-        ArrayList<Building> list = new ArrayList<Building>();
-        Cursor c = _context.getContentResolver().query(BuildingsContentProvider.Columns.CONTENT_URI, BuildingsContentProvider.Columns.ALL_COLUMNS, null, null, null);
+	@Override
+	public List<Building> fetchAll() {
+		ArrayList<Building> list = new ArrayList<Building>();
+		Cursor c = _context.getContentResolver().query(BuildingsContentProvider.Columns.CONTENT_URI, BuildingsContentProvider.Columns.ALL_COLUMNS, null, null, null);
 
-        try {
-            c.moveToFirst();
-            do {
-                Building building = getBuildingFromRow(c);
-                list.add(building);
-            } while (c.moveToNext());
+		try {
+			c.moveToFirst();
+			do {
+				Building building = getBuildingFromRow(c);
+				list.add(building);
+			}
+			while (c.moveToNext());
+		}
+		finally {
+			c.close();
+		}
 
-        } finally {
-            c.close();
-        }
+		return list;
+	}
 
-        return list;
-    }
+	@Override
+	public List<Building> fetchAll(IBuildingSorter sortedBy) {
+		return fetchAll();
+	}
 
-    @Override
-    public List<Building> fetchAll(IBuildingSorter sortedBy) {
-        return fetchAll();
-    }
+	@Override
+	public boolean hasRecords() {
+		// TODO:  there has to be a more efficient way to to this.
+		boolean hasRecords = false;
+		Cursor c = _context.getContentResolver().query(BuildingsContentProvider.Columns.CONTENT_URI, BuildingsContentProvider.Columns.ALL_COLUMNS, null, null, null);
 
-    @Override
-    public boolean hasRecords() {
-        // TODO:  there has to be a more efficient way to to this.
-        boolean hasRecords = false;
-        Cursor c = _context.getContentResolver().query(BuildingsContentProvider.Columns.CONTENT_URI, BuildingsContentProvider.Columns.ALL_COLUMNS, null, null, null);
+		try {
+			c.moveToFirst();
+			long count = 0;
+			do {
+				count++;
+			}
+			while (c.moveToNext());
+			hasRecords = count > 0;
+		}
+		catch (Exception ex) {
+			hasRecords = false;
+		}
+		finally {
+			c.close();
+		}
 
-        try {
-            if (c.moveToFirst()) {
-                long count = 0;
-                do {
-                    count++;
-                }
-                while (c.moveToNext());
-                hasRecords = count > 0;
-            }
-        } catch (Exception ex) {
-            hasRecords = false;
-        } finally {
-            c.close();
-        }
+		return hasRecords;
+	}
 
-        return hasRecords;
-    }
+	private Building getBuildingFromRow(Cursor c) {
+		Building building = new Building();
+		building.setId(c.getLong(0));
+		building.setName(c.getString(1));
+		building.setRowKey(UUID.fromString(c.getString(2)));
+		building.setAddress(c.getString(3));
+		building.setNeighbourHood(c.getString(4));
+		building.setUrl(c.getString(5));
+		building.setConstructionDate(c.getString(6));
 
-    private Building getBuildingFromRow(Cursor c) {
-        Building building = new Building();
-        building.setId(c.getLong(0));
-        building.setName(c.getString(1));
-        building.setRowKey(UUID.fromString(c.getString(2)));
-        building.setAddress(c.getString(3));
-        building.setNeighbourHood(c.getString(4));
-        building.setUrl(c.getString(5));
-        building.setConstructionDate(c.getString(6));
+		String latitude = c.getString(7);
+		String longitude = c.getString(8);
+		building.setLocation(Double.parseDouble(latitude), Double.parseDouble(longitude));
 
-        String latitude = c.getString(8);
-        String longitude = c.getString(7);
-
-        building.setLocation(Double.parseDouble(latitude), Double.parseDouble(longitude));
-        return building;
-    }
+		return building;
+	}
 }
