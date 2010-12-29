7fd55e949d2591e0ef97ddb9eee815fc7988dfd6
Tom Opgenorth <tom@opgenorth.net>
2010-12-29 14:46:23 -0700

    listen to the user location and display it on the list tab.

--- src/net/opgenorth/yeg/buildings/views/BuildingList.java
+++ src/net/opgenorth/yeg/buildings/views/BuildingList.java
@@ -1,55 +1,145 @@
 package net.opgenorth.yeg.buildings.views;
 
 import android.app.ListActivity;
+import android.location.*;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+import net.opgenorth.yeg.buildings.Constants;
 import net.opgenorth.yeg.buildings.R;
-import net.opgenorth.yeg.buildings.data.SQLiteBuildingDataService;
 import net.opgenorth.yeg.buildings.data.IBuildingDataService;
+import net.opgenorth.yeg.buildings.data.SQLiteBuildingDataService;
 import net.opgenorth.yeg.buildings.model.Building;
 import net.opgenorth.yeg.buildings.model.RelativeBuildingLocation;
+import net.opgenorth.yeg.buildings.util.LocationManagerBuilder;
 import net.opgenorth.yeg.buildings.widget.BuildingListAdapter;
 
+import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
 
 public class BuildingList extends ListActivity {
-    private TextView _myGpsLocation;
-    private TextView _buildingCount;
-    private List<RelativeBuildingLocation> _buildingList = new ArrayList<RelativeBuildingLocation>(76);
-    private BuildingListAdapter _buildingListAdapter;
+	private List<RelativeBuildingLocation> _buildingList             = new ArrayList<RelativeBuildingLocation>(76);
+	private LocationListener               _onLocationChangeListener = new MyLocationListener();
+	private LocationManager     _locationManager;
+	private TextView            _myGpsLocation;
+	private TextView            _buildingCount;
+	private BuildingListAdapter _buildingListAdapter;
+	private Location            _currentLocation;
 
-    public void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 
-        super.onCreate(savedInstanceState);
+		super.onCreate(savedInstanceState);
+		initializeView();
 
-        initializeView();
-        displayGpsLocation();
-        getBuildingList();
-        displayBuildingList();
-    }
+		_locationManager = LocationManagerBuilder.createLocationManager()
+				.with(this)
+				.listeningWith(_onLocationChangeListener)
+				.build();
 
-    private void initializeView() {
-        setContentView(R.layout.buildinglist);
-        _myGpsLocation = (TextView) findViewById(R.id.buildinglist_my_gps);
-        _buildingCount = (TextView) findViewById(R.id.buildinglist_building_count);
-    }
+		displayGpsLocation();
+		getBuildingList();
+		displayBuildingList();
+	}
 
-    private void displayGpsLocation() {
-        _myGpsLocation.setText("This is where the user's location would be");
-    }
+	private void initializeView() {
+		setContentView(R.layout.buildinglist);
+		_myGpsLocation = (TextView) findViewById(R.id.buildinglist_my_gps);
+		_buildingCount = (TextView) findViewById(R.id.buildinglist_building_count);
+	}
 
-    private void displayBuildingList() {
-        _buildingCount.setText("Found " + _buildingList.size()+ " buildings.");
-        _buildingListAdapter = new BuildingListAdapter(BuildingList.this, _buildingList);
-        setListAdapter(_buildingListAdapter);
-    }
+	private void displayGpsLocation() {
+		_myGpsLocation.setText("This is where the user's location would be");
+	}
 
-    private void getBuildingList()  {
-        IBuildingDataService svc = new SQLiteBuildingDataService(this);
+	private void displayBuildingList() {
+		_buildingCount.setText("Found " + _buildingList.size() + " buildings.");
+		_buildingListAdapter = new BuildingListAdapter(BuildingList.this, _buildingList);
+		setListAdapter(_buildingListAdapter);
+	}
 
-        for (Building building : svc.fetchAll()) {
-            _buildingList.add(new RelativeBuildingLocation(building, null));
-        }
-    }
+	private void getBuildingList() {
+		IBuildingDataService svc = new SQLiteBuildingDataService(this);
+
+		for (Building building : svc.fetchAll()) {
+			_buildingList.add(new RelativeBuildingLocation(building, null));
+		}
+	}
+
+	private void updateWithNewLocation() {
+		for (RelativeBuildingLocation relativeBuildingLocation : _buildingList) {
+			relativeBuildingLocation.setRelativeLocation(_currentLocation);
+		}
+		Collections.sort(_buildingList);
+		if (_buildingListAdapter == null) {
+			Log.e(Constants.LOG_TAG, "Why isn't there a _buildingListAdapter?");
+		}
+		else {
+			_buildingListAdapter.notifyDataSetChanged();
+		}
+		showMyGpsLocation();
+	}
+
+	private void showMyGpsLocation() {
+		if (_myGpsLocation == null) {
+			return;
+		}
+		String myLocation = "I'm not to sure where you are.";
+		if (_currentLocation != null) {
+			DecimalFormat formatter = new DecimalFormat("###.######");
+			List myList;
+			String lat = formatter.format(_currentLocation.getLatitude());
+			String lon = formatter.format(_currentLocation.getLongitude());
+			String loc = lat + "," + lon;
+			myLocation = "My location: " + loc;
+			Geocoder geoCoder = new Geocoder(this, Locale.getDefault());
+			try {
+				myList = geoCoder.getFromLocation(_currentLocation.getLatitude(), _currentLocation.getLongitude(), 1);
+				if (myList.size() > 0) {
+					Address address = (Address) myList.get(0);
+					myLocation = "My location: " + address.getAddressLine(0) + ", " + address.getLocality();
+				}
+				else {
+					Log.d(Constants.LOG_TAG, "Couldn't translate the location " + loc + " into an address.");
+				}
+			}
+			catch (IOException e) {
+				myLocation = "My location: " + lat + ", " + lon;
+			}
+		}
+		_myGpsLocation.setText(myLocation);
+	}
+
+	private class MyLocationListener implements LocationListener {
+
+		@Override
+		public void onLocationChanged(Location location) {
+			_currentLocation = location;
+			if (location == null) {
+				Log.w(Constants.LOG_TAG, "We have a NULL location for some reason.");
+				return;
+			}
+			Log.d(Constants.LOG_TAG, "new location " + location.getLongitude() + " " + location.getLatitude());
+
+			updateWithNewLocation();
+		}
+
+		@Override
+		public void onStatusChanged(String s, int i, Bundle bundle) {
+			Log.i(Constants.LOG_TAG, "GPS Provider status changed.");
+		}
+
+		@Override
+		public void onProviderEnabled(String s) {
+			Log.i(Constants.LOG_TAG, "GPS Provider is enabled.");
+		}
+
+		@Override
+		public void onProviderDisabled(String s) {
+			Log.i(Constants.LOG_TAG, "GPS Provider is disabled.");
+		}
+	}
 }
--- src/net/opgenorth/yeg/buildings/widget/BuildingListAdapter.java
+++ src/net/opgenorth/yeg/buildings/widget/BuildingListAdapter.java
@@ -13,39 +13,39 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BuildingListAdapter extends ArrayAdapter {
-    private Activity _context;
-    private List<RelativeBuildingLocation> _relativeBuildings;
-    private List<Building> _buildings;
+	private Activity                       _context;
+	private List<RelativeBuildingLocation> _relativeBuildings;
+	private List<Building>                 _buildings;
 
-    public BuildingListAdapter(Activity context, List<RelativeBuildingLocation> relativeBuildings) {
-        super(context, R.layout.historicalbuildingrow, relativeBuildings);
-        _context = context;
-        _relativeBuildings = relativeBuildings;
-        _buildings = new ArrayList<Building>(relativeBuildings.size());
-        for (RelativeBuildingLocation building : relativeBuildings) {
-            _buildings.add(building.getBuilding());
-        }
-    }
+	public BuildingListAdapter(Activity context, List<RelativeBuildingLocation> relativeBuildings) {
+		super(context, R.layout.historicalbuildingrow, relativeBuildings);
+		_context = context;
+		_relativeBuildings = relativeBuildings;
+		_buildings = new ArrayList<Building>(relativeBuildings.size());
+		for (RelativeBuildingLocation building : relativeBuildings) {
+			_buildings.add(building.getBuilding());
+		}
+	}
 
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        View row = convertView;
-        BuildingRowWrapper rowWrapper;
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		View row = convertView;
+		BuildingRowWrapper rowWrapper;
 
-        if (row == null) {
-            LayoutInflater layoutInflater = _context.getLayoutInflater();
-            row = layoutInflater.inflate(R.layout.historicalbuildingrow, null);
-            rowWrapper = new BuildingRowWrapper(row);
-            row.setTag(rowWrapper);
-        } else {
-            rowWrapper = (BuildingRowWrapper) row.getTag();
-        }
+		if (row == null) {
+			LayoutInflater layoutInflater = _context.getLayoutInflater();
+			row = layoutInflater.inflate(R.layout.historicalbuildingrow, null);
+			rowWrapper = new BuildingRowWrapper(row);
+			row.setTag(rowWrapper);
+		}
+		else {
+			rowWrapper = (BuildingRowWrapper) row.getTag();
+		}
 
-        if (!_relativeBuildings.isEmpty()) {
-            RelativeBuildingLocation relativeBuilding = _relativeBuildings.get(position);
-            rowWrapper.display(relativeBuilding);
-        }
-
+		if (!_relativeBuildings.isEmpty()) {
+			RelativeBuildingLocation relativeBuilding = _relativeBuildings.get(position);
+			rowWrapper.display(relativeBuilding);
+		}
 /*
         if (position % 2 == 0) {
             row.setBackgroundResource(R.color.roweven);
@@ -53,7 +53,7 @@ public class BuildingListAdapter extends ArrayAdapter {
             row.setBackgroundResource(R.color.rowodd);
         }
 */
+		return (row);
+	}
 
-        return (row);
-    }
 }
