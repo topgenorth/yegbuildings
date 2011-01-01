afdc09c6a160f697279e4efa0872611096130f26
Tom Opgenorth <tom@opgenorth.net>
2011-01-01 15:36:14 -0700

    removed a dead constant

--- src/net/opgenorth/yeg/buildings/Constants.java
+++ src/net/opgenorth/yeg/buildings/Constants.java
@@ -7,9 +7,9 @@ public final class Constants {
 	public static final String LOG_TAG          = "net.opgenorth.yeg.buildings";
 	public static final String LAST_LAT         = "LastKnownLatitude";
 	public static final String LAST_LON         = "LastKnownLongitude";
+	public static final String LAST_MAP_ZOOM    = "LastMapZoom";
 	public static final int    DEFAULT_LAT      = 53544643;
 	public static final int    DEFAULT_LON      = -113490060;
 	public static final int    DEFAULT_MAP_ZOOM = 14;
-	public static final String LAST_MAP_ZOOM    = "LastMapZoom";
 
 }
