9bc965646561b42dc99635bda4da7856821c862b
Tom Opgenorth <tom@opgenorth.net>
2011-01-04 21:43:30 -0700

    reformatted code.

--- src/net/opgenorth/yeg/buildings/util/ByteArrayResponseHandler.java
+++ src/net/opgenorth/yeg/buildings/util/ByteArrayResponseHandler.java
@@ -3,7 +3,6 @@ package net.opgenorth.yeg.buildings.util;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.StatusLine;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpResponseException;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.util.EntityUtils;
@@ -15,12 +14,14 @@ public class ByteArrayResponseHandler implements ResponseHandler<byte[]> {
 	@Override
 	public byte[] handleResponse(HttpResponse httpResponse) throws IOException {
 		StatusLine statusLine = httpResponse.getStatusLine();
-		if (statusLine.getStatusCode() >= 300)
+		if (statusLine.getStatusCode() >= 300) {
 			throw new HttpResponseException(statusLine.getStatusCode(), statusLine.getReasonPhrase());
+		}
 
 		HttpEntity entity = httpResponse.getEntity();
-		if (entity == null)
+		if (entity == null) {
 			return null;
+		}
 
 		return EntityUtils.toByteArray(entity);
 	}
