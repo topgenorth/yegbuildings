74957e33f203cbb2628ec2cb380e53d48e6a6729
Tom Opgenorth <tom@opgenorth.net>
2011-01-27 18:29:18 -0700

    fixing up AndroidManifest.xml - was not working

--- templates/deploy-to-phone.sh
+++
@@ -1,4 +0,0 @@
-#/bin/sh
-# use this to deploy the application to your phone.  Just replace <DEVICENAME> with the name of your phone.
-adb -s <DEVICENAME> uninstall net.opgenorth.yeg.buildings
-adb -s <DEVICENAME> install ./YegBuildings.apk
