792d2d7be56906ee43b8f214a9707e111254e736
Tom Opgenorth <tom@opgenorth.net>
2011-02-22 21:13:59 -0700

    Created the project using MonoDroid.

---
+++ dotnet/yegbuildings/yegbuildings/Main.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Widget;
+
+namespace yegbuildings
+{
+    [Activity(Label = "YEG Buildings", MainLauncher = true)]
+    public class Main : Activity
+    {
+        private int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            var button = FindViewById<Button>(Resource.Id.MyButton);
+
+            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+        }
+    }
+}
