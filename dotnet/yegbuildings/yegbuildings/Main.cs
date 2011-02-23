ce683c49731b356fdce2b15a2641b561660fa258
Tom Opgenorth <tom@opgenorth.net>
2011-02-22 21:58:05 -0700

    Fix up the namespace.

--- dotnet/yegbuildings/yegbuildings/Main.cs
+++ dotnet/yegbuildings/yegbuildings/Main.cs
@@ -1,26 +1,21 @@
 ﻿using Android.App;
 using Android.OS;
 using Android.Widget;
+using yegbuildings;
 
-namespace yegbuildings
+namespace net.opgenorth.yeg.buildings
 {
     [Activity(Label = "YEG Buildings", MainLauncher = true)]
     public class Main : Activity
     {
-        private int count = 1;
+        private int _count = 1;
 
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
-
-            // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
-
-            // Get our button from the layout resource,
-            // and attach an event to it
             var button = FindViewById<Button>(Resource.Id.MyButton);
-
-            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+            button.Click += delegate { button.Text = string.Format("{0} clicks!", _count++); };
         }
     }
 }
