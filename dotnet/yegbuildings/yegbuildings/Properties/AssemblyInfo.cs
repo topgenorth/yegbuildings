bb93b7a63078e18c4ac7e81e716443208440444b
Tom Opgenorth <tom@opgenorth.net>
2011-03-03 22:13:33 -0700

    Initial commit of the MonoDroid project.

---
+++ dotnet/yegbuildings/yegbuildings/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("YEG Buildings")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("MonoDroid")]
+[assembly: AssemblyCompany("Opgenorth Holdings")]
+[assembly: AssemblyProduct("net.opgenorth.yeg.buildings")]
+[assembly: AssemblyCopyright("Copyright ©  2011")]
+//[assembly: AssemblyTrademark("")]
+//[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("a557ce8c-9dbe-4b93-8fc4-95ffc126cf14")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
