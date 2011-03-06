2a6ae35923d6964bfc3cccdb65d31f86347716b6
Tom Opgenorth <tom@opgenorth.net>
2011-03-06 13:10:23 -0700

    convert CSV to Building.

---
+++ dotnet/yegbuildings/data/CsvLinesToBuildingList.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using net.opgenorth.yeg.buildings.model;
+
+namespace net.opgenorth.yeg.buildings.data
+{
+    public class CsvLinesToBuildingList : ITransmorgifier<IList<string>, IList<Building>>
+    {
+        public static readonly ITransmorgifier<string, Building> LineTransmorgifier = new CsvLineToBuilding();
+        public IList<Building> Transmorgify(IList<string> source)
+        {
+            return source.Select(line => LineTransmorgifier.Transmorgify(line)).Where(building => building != null).ToList();
+        }
+
+    }
+}
---
+++ dotnet/yegbuildings/data/CsvToBuilding.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using net.opgenorth.yeg.buildings.model;
+
+namespace net.opgenorth.yeg.buildings.data
+{
+    public class CsvLineToBuilding : ITransmorgifier<IList<string>, Building>, ITransmorgifier<string, Building>
+    {
+
+        public Building Transmorgify(IList<string> source)
+        {
+            if (source == null)
+            {
+                return null;
+            }
+            if (source.Count < 9)
+            {
+                throw new ArgumentException("There must be at least 8 parts in the IList<string> for conversion.");
+            }
+
+// ReSharper disable UseObjectOrCollectionInitializer
+            var building = new Building();
+// ReSharper restore UseObjectOrCollectionInitializer
+            building.RowKey = Guid.Parse(source[0]);
+            building.Name = source[1].Trim();
+            building.Address = source[2].Trim();
+            building.NeighbourHood = source[3].Trim();
+            building.Url = source[4].Trim();
+            building.ConstructionDate = source[5].Trim();
+            building.Latitude = Double.Parse(source[6]);
+            building.Longitude = Double.Parse(source[7]);
+            return building;
+        }
+
+
+        public Building Transmorgify(string source)
+        {
+            if (String.IsNullOrWhiteSpace(source))
+            {
+                return null;
+            }
+            string[] parts = source.Split(',');
+            return Transmorgify(parts);
+        }
+
+    }
+}
