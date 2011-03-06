74a7cffbb49ce3d1b881fb33889cc1ea7990bec2
Tom Opgenorth <tom@opgenorth.net>
2011-03-06 12:42:55 -0700

    Renamed Activity1 -> Main

--- dotnet/yegbuildings/Main.cs
+++ dotnet/yegbuildings/Main.cs
@@ -5,22 +5,14 @@ using Android.Widget;
 namespace net.opgenorth.yeg.buildings
 {
     [Activity(Label = "YEG Buildings", MainLauncher = true)]
-    public class Activity1 : Activity
+    public class Main : Activity
     {
         private int count = 1;
 
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
-
-            // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
-
-            // Get our button from the layout resource,
-            // and attach an event to it
-//            var button = FindViewById<Button>(Resource.Id.MyButton);
-//
-//            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
         }
     }
 }
