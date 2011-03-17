6c6b6406d63553811d155207989b3e1a736542a1
Tom Opgenorth <tom@opgenorth.net>
2011-03-16 22:17:36 -0600

    take the PDFs, extract images and convert to HTML.

--- pdf/converttotext.rb
+++ pdf/converttotext.rb
@@ -1,9 +1,18 @@
 #!/usr/bin/ruby
 
-for txtfile in Dir.glob("*.txt")
-	File.delete(txtfile)
+Dir.glob("*.txt") do |file| File.delete(file) end
+Dir.glob("*.jpg") do |file| File.delete(file) end
+
+Dir.glob("*.pdf") do |file|
+	basename = File.basename(file,'.*')
+	if (File.directory?(basename))
+		Dir.glob("./#{basename}/*.*") do |file2| File.delete(file2) end
+	else
+		Dir.mkdir basename unless File.directory?(basename)
+	end
+	system("pdftotext", "-htmlmeta", file, "./#{basename}/#{basename}.html")
+	system("pdfimages", "-j",  file, "./#{basename}/")
+#	File.copy file, "./#{basename}/file"
+	puts "Converted #{file} to text and extracted images to #{basename}."
 end
 
-for pdffile in Dir.glob("*.pdf")
-	system("pdftotext", pdffile)
-end
