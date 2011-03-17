fbae166e41e543896d853737405ab1ca8ab73a03
Tom Opgenorth <tom@opgenorth.net>
2011-03-16 20:48:12 -0600

    Simplifying * Only use the building map. * rolled back the version name to 1.9.0.0

--- java/src/net/opgenorth/yeg/buildings/views/BuildingMap.java
+++ java/src/net/opgenorth/yeg/buildings/views/BuildingMap.java
@@ -6,6 +6,8 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -24,208 +26,214 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BuildingMap extends MapActivity implements LocationListener {
-	private LocationManager   _locationManager;
-	private MapView           _map;
-	private MyLocationOverlay _myLocationOverlay;
-	private ArrayList<RelativeBuildingLocation> _buildingList   = new ArrayList<RelativeBuildingLocation>(76);
-	private ActivityHelper                      _activityHelper = new ActivityHelper(this);
+    private LocationManager _locationManager;
+    private MapView _map;
+    private MyLocationOverlay _myLocationOverlay;
+    private ArrayList<RelativeBuildingLocation> _buildingList = new ArrayList<RelativeBuildingLocation>(76);
+    private ActivityHelper _activityHelper = new ActivityHelper(this);
+    private final Handler _handler = new Handler();
+    private final long LOCATION_LISTENER_TIMEOUT_DURATION = 1 * 60 * 1000; // time out in milliseconds.
 
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		initializeContentView();
-		initializeMapView();
-		getBuildingList();
-		initializeHistoricalBuildingsOverlay();
-		initializeMyLocationOverlay();
-		showMyLocationOnMap();
-	}
+    private final Runnable _unregisterLocationListener = new Runnable() {
+        @Override
+        public void run() {
+            _myLocationOverlay.disableMyLocation();
+            _locationManager.removeUpdates(BuildingMap.this);
+            Log.d(Constants.LOG_TAG, "Stop requesting location updates - the timeout has passed.");
+        }
+    };
 
-	private void initializeContentView() {
-		if (_activityHelper.isDebug()) {
-			Log.v(Constants.LOG_TAG, "Debuggable == TRUE, using mapofallbuildings_debug.xml.");
-			setContentView(R.layout.mapofallbuildings_debug);
-		}
-		else {
-			Log.v(Constants.LOG_TAG, "Debuggable == FALSE, using mapofallbuildings_production.xml.");
-			setContentView(R.layout.mapofallbuildings_production);
-		}
-		_map = (MapView) findViewById(R.id.mapofallbuildings);
 
-	}
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initializeContentView();
+        initializeMapView();
+        getBuildingList();
+        initializeHistoricalBuildingsOverlay();
+        initializeMyLocationOverlay();
+        showMyLocationOnMap();
+    }
 
-	@Override
-	protected void onStop() {
+    private void initializeContentView() {
+        if (_activityHelper.isDebug()) {
+            Log.v(Constants.LOG_TAG, "Debuggable == TRUE, using mapofallbuildings_debug.xml.");
+            setContentView(R.layout.mapofallbuildings_debug);
+        } else {
+            Log.v(Constants.LOG_TAG, "Debuggable == FALSE, using mapofallbuildings_production.xml.");
+            setContentView(R.layout.mapofallbuildings_production);
+        }
+        _map = (MapView) findViewById(R.id.mapofallbuildings);
 
-		_locationManager.removeUpdates(this);
-		_myLocationOverlay.disableMyLocation();
+    }
 
-		SharedPreferences settings = getPreferences(MODE_PRIVATE);
-		SharedPreferences.Editor settingsEditor = settings.edit();
-		settingsEditor.putInt(Constants.PREF_LAST_MAP_ZOOM, _map.getZoomLevel());
+    @Override
+    protected void onStop() {
+        super.onStop();
 
-		// this should get the point that is in the middle of the map.
-		GeoPoint p = _map.getProjection().fromPixels(_map.getWidth() / 2, _map.getHeight() / 2);
-		settingsEditor.putInt(Constants.PREF_LAST_LAT, p.getLatitudeE6());
-		settingsEditor.putInt(Constants.PREF_LAST_LON, p.getLongitudeE6());
+        _locationManager.removeUpdates(this);
+        _myLocationOverlay.disableMyLocation();
 
-		settingsEditor.commit();
 
-		super.onStop();
-	}
 
-	@Override
-	protected void onStart() {
-		super.onStart();
-		_locationManager = LocationManagerBuilder.createLocationManager()
-				.with(this)
-				.listeningWith(this)
-				.build();
-	}
+        // TODO: make this happen off the UI thread.
+        // Save the centre of the map and the zoom level for the next time that the user starts up the application.
+        GeoPoint p = _map.getProjection().fromPixels(_map.getWidth() / 2, _map.getHeight() / 2);
 
-	@Override
-	protected void onResume() {
-		super.onResume();
-/*
-		_locationManager = LocationManagerBuilder.createLocationManager()
-				.with(this)
-				.listeningWith(this)
-				.build();
-		_myLocationOverlay.enableMyLocation();
-		_myLocationOverlay.enableCompass();
-*/
-	}
+        SharedPreferences settings = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor settingsEditor = settings.edit();
+        settingsEditor.putInt(Constants.PREF_LAST_MAP_ZOOM, _map.getZoomLevel());
+        settingsEditor.putInt(Constants.PREF_LAST_LAT, p.getLatitudeE6());
+        settingsEditor.putInt(Constants.PREF_LAST_LON, p.getLongitudeE6());
+        settingsEditor.commit();
 
-	@Override
-	protected void onPause() {
-		super.onPause();
-/*
-		_locationManager.removeUpdates(this);
-		_myLocationOverlay.disableMyLocation();
-		_myLocationOverlay.disableCompass();
-*/
-	}
+    }
 
-	private void initializeMapView() {
-		SharedPreferences settings = getPreferences(MODE_PRIVATE);
+    @Override
+    protected void onStart() {
+        super.onStart();
+        _locationManager = LocationManagerBuilder.createLocationManager()
+                .with(this)
+                .listeningWith(this)
+                .build();
+    }
 
-		int latitude = settings.getInt(Constants.PREF_LAST_LAT, Constants.PREF_DEFAULT_LAT);
-		int longitude = settings.getInt(Constants.PREF_LAST_LON, Constants.PREF_DEFAULT_LON);
-		GeoPoint centerOfMap = new GeoPoint(latitude, longitude);
-		_map.getController().animateTo(centerOfMap);
+    @Override
+    protected void onResume() {
+        super.onResume();
+        _handler.removeCallbacks(_unregisterLocationListener);
+        _locationManager = LocationManagerBuilder.createLocationManager()
+                .with(this)
+                .listeningWith(this)
+                .build();
+    }
 
-		int mapZoom = settings.getInt(Constants.PREF_LAST_MAP_ZOOM, Constants.PREF_DEFAULT_MAP_ZOOM);
-		_map.getController().setZoom(mapZoom);
-		_map.setClickable(true);
-		_map.setLongClickable(true);
-		_map.setBuiltInZoomControls(true);
-	}
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(Constants.LOG_TAG, "onPause - we'll turn off the location listener after " + LOCATION_LISTENER_TIMEOUT_DURATION/1000 + " seconds.");
+        _handler.postDelayed(_unregisterLocationListener, LOCATION_LISTENER_TIMEOUT_DURATION);
+    }
 
-	private void initializeMyLocationOverlay() {
-		_myLocationOverlay = new MyLocationOverlay(this, _map);
-		_myLocationOverlay.enableMyLocation();
-		_map.getOverlays().add(_myLocationOverlay);
-	}
+    private void initializeMapView() {
+        SharedPreferences settings = getPreferences(MODE_PRIVATE);
 
-	private void initializeHistoricalBuildingsOverlay() {
-		Drawable buildingMarker = getResources().getDrawable(R.drawable.building_medium);
-		buildingMarker.setBounds(0, 0, buildingMarker.getIntrinsicWidth(), buildingMarker.getIntrinsicHeight());
-		_map.getOverlays().add(new BuildingsOverlay(buildingMarker));
-	}
+        int latitude = settings.getInt(Constants.PREF_LAST_LAT, Constants.PREF_DEFAULT_LAT);
+        int longitude = settings.getInt(Constants.PREF_LAST_LON, Constants.PREF_DEFAULT_LON);
+        GeoPoint centerOfMap = new GeoPoint(latitude, longitude);
+        _map.getController().animateTo(centerOfMap);
 
-	@Override
-	protected boolean isRouteDisplayed() {
-		return false;
-	}
+        int mapZoom = settings.getInt(Constants.PREF_LAST_MAP_ZOOM, Constants.PREF_DEFAULT_MAP_ZOOM);
+        _map.getController().setZoom(mapZoom);
+        _map.setClickable(true);
+        _map.setLongClickable(true);
+        _map.setBuiltInZoomControls(true);
+    }
 
-	@Override
-	public void onLocationChanged(Location location) {
-	}
+    private void initializeMyLocationOverlay() {
+        _myLocationOverlay = new MyLocationOverlay(this, _map);
+        _myLocationOverlay.enableMyLocation();
+        _map.getOverlays().add(_myLocationOverlay);
+    }
 
-	@Override
-	public void onStatusChanged(String s, int i, Bundle bundle) {
-		Log.v(Constants.LOG_TAG, "GPS Provider status changed.");
-	}
+    private void initializeHistoricalBuildingsOverlay() {
+        Drawable buildingMarker = getResources().getDrawable(R.drawable.building_medium);
+        buildingMarker.setBounds(0, 0, buildingMarker.getIntrinsicWidth(), buildingMarker.getIntrinsicHeight());
+        _map.getOverlays().add(new BuildingsOverlay(buildingMarker));
+    }
 
-	@Override
-	public void onProviderEnabled(String s) {
-		Log.v(Constants.LOG_TAG, "GPS Provider is enabled.");
-	}
+    @Override
+    protected boolean isRouteDisplayed() {
+        return false;
+    }
 
-	@Override
-	public void onProviderDisabled(String s) {
-		Log.v(Constants.LOG_TAG, "GPS Provider is disabled.");
-	}
+    @Override
+    public void onLocationChanged(Location location) {
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		new MenuInflater(getApplication()).inflate(R.menu.map, menu);
-		return (super.onCreateOptionsMenu(menu));
-	}
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+        Log.v(Constants.LOG_TAG, "GPS Provider status changed.");
+    }
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		int itemId = item.getItemId();
-		if (R.id.map_menu_showMyLocation == itemId) {
-			showMyLocationOnMap();
-		}
-		return super.onOptionsItemSelected(item);
-	}
+    @Override
+    public void onProviderEnabled(String s) {
+        Log.v(Constants.LOG_TAG, "GPS Provider is enabled.");
+    }
 
-	private void showMyLocationOnMap() {
-		GeoPoint myLocation = _myLocationOverlay.getMyLocation();
-		if (myLocation == null) {
-			Toast.makeText(this, "Can't seem to figure out your location.", Toast.LENGTH_SHORT);
-			Log.i(Constants.LOG_TAG, "Can't figure out the user's location.");
-		}
-		else {
-			_map.invalidate();
-			_myLocationOverlay.enableMyLocation();
-			_map.getController().animateTo(myLocation);
-		}
-	}
+    @Override
+    public void onProviderDisabled(String s) {
+        Log.v(Constants.LOG_TAG, "GPS Provider is disabled.");
+    }
 
-	private void getBuildingList() {
-		// TODO: duplication with BuildingList.java
-		IBuildingDataService svc = new SQLiteBuildingDataService(this);
-		for (Building building : svc.fetchAll()) {
-			_buildingList.add(new RelativeBuildingLocation(building, null));
-		}
-	}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        new MenuInflater(getApplication()).inflate(R.menu.map, menu);
+        return (super.onCreateOptionsMenu(menu));
+    }
 
-	private class BuildingsOverlay extends ItemizedOverlay<OverlayItem> {
-		private Drawable _marker;
-		private List<OverlayItem> _items = new ArrayList<OverlayItem>();
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        if (R.id.map_menu_showMyLocation == itemId) {
+            showMyLocationOnMap();
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
-		public BuildingsOverlay(Drawable drawable) {
-			super(drawable);
-			_marker = drawable;
-			boundCenterBottom(drawable);
-			loadItemsFromBuildings();
-			populate();
-		}
+    private void showMyLocationOnMap() {
+        GeoPoint myLocation = _myLocationOverlay.getMyLocation();
+        if (myLocation == null) {
+            Toast.makeText(this, "Can't seem to figure out your location.", Toast.LENGTH_SHORT);
+            Log.i(Constants.LOG_TAG, "Can't figure out the user's location.");
+        } else {
+            _map.invalidate();
+            _myLocationOverlay.enableMyLocation();
+            _map.getController().animateTo(myLocation);
+        }
+    }
 
-		private void loadItemsFromBuildings() {
-			for (RelativeBuildingLocation location : _buildingList) {
-				OverlayItem overlay = location.getOverlayItem();
-				Log.d(Constants.LOG_TAG, "Adding location " + location.toString());
-				_items.add(overlay);
-			}
-		}
+    private void getBuildingList() {
+        // TODO: duplication with BuildingList.java
+        IBuildingDataService svc = new SQLiteBuildingDataService(this);
+        for (Building building : svc.fetchAll()) {
+            _buildingList.add(new RelativeBuildingLocation(building, null));
+        }
+    }
 
-		@Override
-		protected OverlayItem createItem(int i) {
-			return _items.get(i);
-		}
+    private class BuildingsOverlay extends ItemizedOverlay<OverlayItem> {
+        private Drawable _marker;
+        private List<OverlayItem> _items = new ArrayList<OverlayItem>();
 
-		@Override
-		public int size() {
-			return _items.size();
-		}
+        public BuildingsOverlay(Drawable drawable) {
+            super(drawable);
+            _marker = drawable;
+            boundCenterBottom(drawable);
+            loadItemsFromBuildings();
+            populate();
+        }
 
-		@Override
-		protected boolean onTap(int i) {
-			Toast.makeText(BuildingMap.this, _items.get(i).getSnippet(), Toast.LENGTH_LONG).show();
-			return true;
-		}
-	}
+        private void loadItemsFromBuildings() {
+            for (RelativeBuildingLocation location : _buildingList) {
+                OverlayItem overlay = location.getOverlayItem();
+                Log.d(Constants.LOG_TAG, "Adding location " + location.toString());
+                _items.add(overlay);
+            }
+        }
+
+        @Override
+        protected OverlayItem createItem(int i) {
+            return _items.get(i);
+        }
+
+        @Override
+        public int size() {
+            return _items.size();
+        }
+
+        @Override
+        protected boolean onTap(int i) {
+            Toast.makeText(BuildingMap.this, _items.get(i).getSnippet(), Toast.LENGTH_LONG).show();
+            return true;
+        }
+    }
 }
