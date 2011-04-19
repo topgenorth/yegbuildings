059b11fb7a6bdc6e0a3fbd127de96e50160b6958
Tom Opgenorth <tom@opgenorth.net>
2011-04-18 22:36:55 -0600

    Updated project to Google APIS 2.2, adding in junit 4.8.2 JAR.

--- java/src/test/java/net/opgenorth/yeg/buildings/tests/util/BuildingContentValuesTransmorgifierTests.java
+++ java/src/test/java/net/opgenorth/yeg/buildings/tests/util/BuildingContentValuesTransmorgifierTests.java
@@ -1,12 +1,19 @@
 package net.opgenorth.yeg.buildings.tests.util;
 
 import android.content.ContentValues;
+import com.xtremelabs.robolectric.RobolectricTestRunner;
+import net.opgenorth.yeg.buildings.data.BuildingsContentProvider;
 import net.opgenorth.yeg.buildings.model.Building;
 import net.opgenorth.yeg.buildings.util.BuildingContentValuesTransmorgifier;
 import org.junit.*;
+import org.junit.runner.RunWith;
+
 
 import java.util.UUID;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
 
+@RunWith(RobolectricTestRunner.class)
 public class BuildingContentValuesTransmorgifierTests {
 
     private ContentValues _contentValues;
@@ -27,7 +34,12 @@ public class BuildingContentValuesTransmorgifierTests {
         _contentValues = transmorgifier.transmorgify(_building);
     }
     @Test
-    public void shouldConvertAddress() {
+    public void shouldPutConstructionDateIntoContentValues() {
+        assertThat("2002", equalTo(_contentValues.getAsString(BuildingsContentProvider.Columns.CONSTRUCTION_DATE)));
+    }
 
+    @Test()
+    public void shouldPutAddressIntoContentValues() {
+        assertThat("66 Mockingbird Lane", equalTo(_contentValues.getAsString(BuildingsContentProvider.Columns.ADDRESS)));
     }
 }
