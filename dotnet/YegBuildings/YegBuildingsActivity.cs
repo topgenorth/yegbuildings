39e75619af5e7b9e0594d32379bebfeba6967753
Tom Opgenorth <tom@opgenorth.net>
2012-01-23 18:38:35 -0700

    Committing the M4A 4.0.3 project w/Google maps

---
+++ dotnet/YegBuildings/YegBuildingsActivity.cs
@@ -0,0 +1,17 @@
+﻿#region
+using Android.App;
+using Android.OS;
+
+#endregion
+
+namespace net.opgenorth.m4a.yegbuildings
+{
+    [Activity(Label = "@string/ApplicationName", MainLauncher = true, Icon = "@drawable/icon")]
+    public class YegBuildingsActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+        }
+    }
+}
