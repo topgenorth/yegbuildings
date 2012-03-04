4e52568e10d3bfaba4a219be4c37091b7882e82e
Tom Opgenorth <tom@opgenorth.net>
2012-03-04 15:38:17 -0700

    Getting rid of all the Java code.

--- java/src/main/java/net/opgenorth/yeg/buildings/model/IBuildingSorter.java
+++
@@ -1,7 +0,0 @@
-package net.opgenorth.yeg.buildings.model;
-
-import java.util.List;
-
-public interface IBuildingSorter {
-	List<Building> sortList(List<Building> buildings);
-}
--- java/src/main/java/net/opgenorth/yeg/buildings/model/IBundleModel.java
+++
@@ -1,9 +0,0 @@
-package net.opgenorth.yeg.buildings.model;
-
-import android.os.Bundle;
-
-public interface IBundleModel<T> {
-	android.os.Bundle toBundle(T obj);
-
-	T fromBundle(Bundle bundle);
-}
