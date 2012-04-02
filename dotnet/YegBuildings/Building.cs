f07559173210e6b959ffbd887f1074435b56a96b
Tom Opgenorth <tom@opgenorth.net>
2012-04-01 23:03:18 -0600

    Creating some classes to load the buildings from the CSV.

---
+++ dotnet/YegBuildings/Building.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace net.opgenorth.yegbuildings.m4a
+{
+    public class Building
+    {
+        public Guid EntityId { get; set; }
+        public String Name { get; set;  }
+        public String Address { get; set;  }
+        public String ConstructionDate { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set;  }
+    }
+}
