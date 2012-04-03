a6fec83ec10a548e9c745fb512aebb737115659d
Tom Opgenorth <tom@opgenorth.net>
2012-04-02 21:56:49 -0600

    Created a ListFragment to hold the list of buildings.

---
+++ dotnet/YegBuildings/BuildingListFragment.cs
@@ -0,0 +1,53 @@
+using System.Linq;
+using Android.App;
+using Android.OS;
+using Android.Views;
+using Android.Widget;
+
+namespace net.opgenorth.yegbuildings.m4a
+{
+    public class BuildingListFragment : ListFragment
+    {
+        private int _selectedBuildingIndex;
+
+        public override void OnActivityCreated(Bundle savedInstanceState)
+        {
+            base.OnActivityCreated(savedInstanceState);
+            var buildingNames = from building in Activity.Buildings() 
+                                select building.Name;
+
+            var adapter = new ArrayAdapter(Activity, Android.Resource.Layout.SimpleListItemChecked, buildingNames.ToList());
+            ListAdapter = adapter;
+
+            if (savedInstanceState != null)
+            {
+                _selectedBuildingIndex = savedInstanceState.GetInt("selected_building_index", -1);
+            }
+            ListView.ChoiceMode = ChoiceMode.Single;
+            ShowBuilding(_selectedBuildingIndex);
+        }
+
+        public override void OnListItemClick(ListView l, View v, int position, long id)
+        {
+            base.OnListItemClick(l, v, position, id);
+            ShowBuilding(position);
+        }
+
+        public override void OnSaveInstanceState(Bundle outState)
+        {
+            base.OnSaveInstanceState(outState);
+            outState.PutInt("selected_building_index", _selectedBuildingIndex);
+        }
+
+        private void ShowBuilding(int position)
+        {
+            _selectedBuildingIndex = position;
+            if (position < 0)
+            {
+                // todo zoom to the current location of device.
+                return;
+            } 
+            ListView.SetItemChecked(position, true);
+        }
+    }
+}
