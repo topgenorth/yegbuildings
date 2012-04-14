207f3ececf3b45e22fa4f17f7ad4d65132f6c7e2
Tom Opgenorth <tom@opgenorth.net>
2012-04-14 09:28:48 -0600

    Moved the solution file up one directory, renamed the folder for the Android project.

---
+++ dotnet/YegBuildings.Android/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("YegBuildings")]
+[assembly: AssemblyDescription("Historical Buildings in the Edmonton Area.")]
+#if DEBUG
+[assembly: AssemblyConfiguration("DEBUG")]
+#endif
+
+#if RELEASE
+
+[assembly: AssemblyConfiguration("RELEASE")]
+#endif
+
+[assembly: AssemblyCompany("Tom Opgenorth")]
+[assembly: AssemblyProduct("YegBuildings")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("a557ce8c-9dbe-4b93-8fc4-95ffc126cf14")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
---
+++ dotnet/YegBuildings.Android/YegBuildingsActivity.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Android.App;
+using Android.GoogleMaps;
+using Android.OS;
+
+namespace net.opgenorth.yegbuildings.m4a
+{
+    [Activity(Label = "@string/app_name", MainLauncher = true, Icon = "@drawable/icon")]
+    public class YegBuildingsActivity : MapActivity
+    {
+        private List<Building> _buildings;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+
+            var loader = new LoadBuildingsFromAssets(this);
+            _buildings = loader.GetBuildings();
+        }
+
+
+        internal List<Building> Buildings
+        {
+            get { return _buildings; }
+        }
+
+        protected override bool IsRouteDisplayed
+        {
+            get { return false; }
+        }
+    }
+}
