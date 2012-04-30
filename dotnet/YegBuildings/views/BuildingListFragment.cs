6dc4a25cc2c1acd0a658958e3a723201078aa36e
Tom Opgenorth <tom@opgenorth.net>
2012-04-29 23:47:16 -0600

    Putting a bit more information into the list view (like the address).

--- dotnet/YegBuildings/views/BuildingListFragment.cs
+++ dotnet/YegBuildings/views/BuildingListFragment.cs
@@ -1,5 +1,8 @@
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using Android.App;
+using Android.Content;
 using Android.OS;
 using Android.Views;
 using Android.Widget;
@@ -7,6 +10,36 @@ using net.opgenorth.yegbuildings.m4a.model;
 
 namespace net.opgenorth.yegbuildings.m4a.views
 {
+    public class BuildingArrayAdapter : ArrayAdapter<Building>
+    {
+        private Context _context;
+        private readonly List<Building> _buildings;
+
+        public BuildingArrayAdapter(Context context, List<Building> buildings) : base(context, Resource.Layout.buildingrow, buildings)
+        {
+            if (buildings == null)
+            {
+                throw new ArgumentNullException("buildings", "Must provide a list of buildings.");
+            }
+            _context = context;
+            _buildings = buildings;
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            var inflater = (LayoutInflater) Context.GetSystemService(Context.LayoutInflaterService);
+            var row = inflater.Inflate(Resource.Layout.buildingrow, parent, false);
+
+            var name = row.FindViewById<TextView>(Resource.Id.buildingname);
+            var address = row.FindViewById<TextView>(Resource.Id.buildingaddress);
+            var building = _buildings[position];
+            name.Text = building.Name;
+            address.Text = building.Address;
+
+            return row;
+        }
+    }
+
     public class BuildingListFragment : ListFragment
     {
         private int _selectedBuildingIndex;
@@ -14,11 +47,7 @@ namespace net.opgenorth.yegbuildings.m4a.views
         public override void OnActivityCreated(Bundle savedInstanceState)
         {
             base.OnActivityCreated(savedInstanceState);
-            var buildingNames = from building in Activity.Buildings() 
-                                select building.Name;
-
-            var adapter = new ArrayAdapter(Activity, Android.Resource.Layout.SimpleListItemChecked, buildingNames.ToList());
-            ListAdapter = adapter;
+            ListAdapter = new BuildingArrayAdapter(Activity, Activity.Buildings()); ;
 
             if (savedInstanceState != null)
             {
@@ -47,7 +76,7 @@ namespace net.opgenorth.yegbuildings.m4a.views
             {
                 // todo zoom to the current location of device.
                 return;
-            } 
+            }
             ListView.SetItemChecked(position, true);
             var building = Activity.GetBuilding(position);
 
